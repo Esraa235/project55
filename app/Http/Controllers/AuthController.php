533dddc9b5b5bd9582e84622e00f057204728bbe
Esraa235 <sbesraa53@gmail.com>
2024-05-08 22:06:32 +0300

    first commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
       if (Auth::attempt($credentials)) {
         $request->session()->regenerate();
 
-        return redirect()->route('projects.index');
+        return redirect()->route('app');
       }
       return back()->withErrors(['email' => 'Invalids']);
     } 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\AdminController;
+
 // use App\Http\Controllers\Admin\
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +20,7 @@ use App\Http\Controllers\AuthController;
 //     return view('welcome');
 // });
 Route::middleware(['auth'])->group(function(){
-//     Route::resource('students');
-//     Route::resource('projects');
-//     Route::resource('');
+
 Route::get('/settings/password',[AuthController::class,'showChangePassword'])->name('change-password');
 Route::put('/settings/password',[AuthController::class,'updatePassword'])->name('update-password');
 
